3250c77f90ee61145343e3b6943bbfe1210bd1f5
jaguar4321 <ukrsasha4321@gmail.com>
2025-08-09 19:44:43 +0300

    w

--- app/config.py
+++ app/config.py
@@ -1,1 +1,6 @@
-DB_URL = "postgresql+psycopg2://postgres:jaguar@localhost:5432/population_db"
+import os
+
+DB_URL = os.getenv(
+    "DB_URL",
+    "postgresql+psycopg2://postgres:jaguar@localhost:5432/population_db"
+)
