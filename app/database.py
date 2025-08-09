375cdfe796962957d8f9413f32744d024718d985
jaguar4321 <ukrsasha4321@gmail.com>
2025-08-09 20:21:45 +0300

    w

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 def get_engine():
     db_url = os.getenv(
         "DB_URL",
-        # "postgresql+psycopg2://postgres:jaguar@localhost:5432/population_db"
+        "postgresql+psycopg2://postgres:jaguar@localhost:5432/population_db"
     )
     return create_engine(db_url)
 
