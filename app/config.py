d9562b06018c0d3be8f31b78757fe8e5086c4665
jaguar4321 <ukrsasha4321@gmail.com>
2025-08-09 19:56:31 +0300

    w

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DB_URL = os.getenv(
-    "DB_URL",
-    "postgresql+psycopg2://postgres:jaguar@localhost:5432/population_db"
-)
--- app/database.py
+++ app/database.py
@@ -1,9 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config import DB_URL
 
 Base = declarative_base()
-engine = create_engine(DB_URL)
+
+def get_engine():
+    db_url = os.getenv(
+        "DB_URL",
+        "postgresql+psycopg2://postgres:jaguar@localhost:5432/population_db"
+    )
+    return create_engine(db_url)
+
+engine = get_engine()
 SessionLocal = sessionmaker(bind=engine)
 
 def init_db():
